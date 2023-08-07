7818dffbe2fc55d8ba7bff87d5aed8de9d5d64c8
William Coutinho <william.csouza@hotmail.com>
2023-08-06 23:32:45 -0300

    SellerDao findById method + Minor changes

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,10 +1,25 @@
 package model.dao;
 
+import db.DB;
 import model.dao.impl.SellerDaoJDBC;
 
 public class DaoFactory {
 
-    public static SellerDao createSellerDao(){
-        return new SellerDaoJDBC();
+    /*
+     * A ideia de usar um Factory é de não expor implementação.
+     * Então temos a interface EntidadeDAO, que terá seus métodos obrigatórios.
+     * Depois temos um pacote de Implementações (Impl) com as classes extendendo as interfaces
+     * Essas Implementações pode ser do tipo JDBC, Hibernate, etc.
+     * Depois temos a DAO factory que retorna uma instância da implementação mas do tipo Interface (Up-Scalling).
+     * E por quê não instanciar diretamente a implementação? Para ajudar no desacoplamento e manutenção.
+     * Ex.: se tivermos que mudar a forma de implementação de JDBC para Hibernate, imagina a quantidade de
+     *  locais que teria que ser alterado? Se todos instanciam o Factory, basta mudar lá.
+     * Isso traz o princípio de que cada classe é responsável por si, ou seja, eu como Program
+     *   quero uma instância de SellerDao para iniciar as mexidas na base,
+     *   e não quero saber como é implementado e por quem ou onde.
+     * Nota: Injeção de Dependência
+     * */
+    public static SellerDao createSellerDao() {
+        return new SellerDaoJDBC(DB.getConnection());
     }
 }
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -1,11 +1,24 @@
 package model.dao.impl;
 
+import db.DB;
+import db.DBException;
+import entities.Department;
 import entities.Seller;
 import model.dao.SellerDao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 public class SellerDaoJDBC implements SellerDao {
+    private Connection conn;
+
+    public SellerDaoJDBC(Connection conn) {
+        this.conn = conn;
+    }
+
     @Override
     public void insert(Seller obj) {
 
@@ -23,7 +36,35 @@ public class SellerDaoJDBC implements SellerDao {
 
     @Override
     public Seller findById(Integer id) {
-        return null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+
+        try {
+            ps = conn.prepareStatement("SELECT seller.*, department.Name as DepName "
+                    + "FROM seller INNER JOIN department "
+                    + "ON seller.DepartmentId = department.Id "
+                    + "WHERE seller.Id = ?");
+
+            ps.setInt(1, id);
+            rs = ps.executeQuery();
+
+            if (rs.next()) {
+                Department dep = new Department(rs.getInt("DepartmentId"), rs.getString("DepName"));
+                return new Seller(
+                        rs.getInt("Id"),
+                        rs.getString("Name"),
+                        rs.getString("Email"),
+                        rs.getDouble("BaseSalary"),
+                        rs.getDate("BirthDate").toLocalDate(),
+                        dep);
+            }
+            return null;
+        } catch (SQLException e) {
+            throw new DBException(e.getMessage());
+        } finally {
+            DB.closeStatement(ps);
+            DB.closeResultSet(rs);
+        }
     }
 
     @Override
