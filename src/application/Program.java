27ed935997387baccd20e8d216177fde6172029a
William Coutinho <william.csouza@hotmail.com>
2023-08-08 00:16:08 -0300

    Added update seller method

--- src/application/Program.java
+++ src/application/Program.java
@@ -4,38 +4,47 @@ import entities.Department;
 import entities.Seller;
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
+import utils.Util;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.Random;
 
 public class Program {
     public static void main(String[] args) {
         int repeatCount = 30;
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
-        System.out.println("=".repeat(repeatCount) + "TEST 1: seller findById" + "=".repeat(repeatCount));
-        SellerDao sellerDaoTest1 = DaoFactory.createSellerDao();
-        Seller seller = sellerDaoTest1.findById(3);
-        System.out.println(seller);
+//        System.out.println("=".repeat(repeatCount) + "TEST 1: seller findById" + "=".repeat(repeatCount));
+//        SellerDao sellerDaoTest1 = DaoFactory.createSellerDao();
+//        Seller seller = sellerDaoTest1.findById(3);
+//        System.out.println(seller);
+//
+//        System.out.println("=".repeat(repeatCount) + "TEST 2: seller findByDepartment" + "=".repeat(repeatCount));
+//        SellerDao sellerDaoTest2 = DaoFactory.createSellerDao();
+//        List<Seller> sellersTest2 = sellerDaoTest2.findByDepartment(new Department(2, "Electronics"));
+//        sellersTest2.forEach(System.out::println);
+//        System.out.println("Instances Department: " + Department.getNumOfInstances());
+//
+//        System.out.println("=".repeat(repeatCount) + "TEST 3: seller findAll" + "=".repeat(repeatCount));
+//        SellerDao sellerDaoTest3 = DaoFactory.createSellerDao();
+//        List<Seller> sellersTest3 = sellerDaoTest3.findAll();
+//        sellersTest3.forEach(System.out::println);
+//        System.out.println("Instances Department: " + Department.getNumOfInstances());
+//
+//        System.out.println("=".repeat(repeatCount) + "TEST 4: seller insert" + "=".repeat(repeatCount));
+//        SellerDao sellerDaoTest4 = DaoFactory.createSellerDao();
+//        Seller seller4 = new Seller(null, "Greg", "greg@gmail.com", 4000.0,
+//                LocalDate.parse("01/03/1990", dtf), new Department(2, "Electronics"));
+//        sellerDaoTest4.insert(seller4);
+//        System.out.println("Inserted! New id = " + seller4.getId());
 
-        System.out.println("=".repeat(repeatCount) + "TEST 2: seller findByDepartment" + "=".repeat(repeatCount));
-        SellerDao sellerDaoTest2 = DaoFactory.createSellerDao();
-        List<Seller> sellersTest2 = sellerDaoTest2.findByDepartment(new Department(2, "Electronics"));
-        sellersTest2.forEach(System.out::println);
-        System.out.println("Instances Department: " + Department.getNumOfInstances());
-
-        System.out.println("=".repeat(repeatCount) + "TEST 3: seller findAll" + "=".repeat(repeatCount));
-        SellerDao sellerDaoTest3 = DaoFactory.createSellerDao();
-        List<Seller> sellersTest3 = sellerDaoTest3.findAll();
-        sellersTest3.forEach(System.out::println);
-        System.out.println("Instances Department: " + Department.getNumOfInstances());
-
-        System.out.println("=".repeat(repeatCount) + "TEST 4: seller insert" + "=".repeat(repeatCount));
-        SellerDao sellerDaoTest4 = DaoFactory.createSellerDao();
-        Seller seller4 = new Seller(null, "Greg", "greg@gmail.com", 4000.0,
-                LocalDate.parse("01/03/1990", dtf), new Department(2, "Electronics"));
-        sellerDaoTest4.insert(seller4);
-        System.out.println("Inserted! New id = " + seller4.getId());
+        System.out.println("=".repeat(repeatCount) + "TEST 5: seller update" + "=".repeat(repeatCount));
+        SellerDao sellerDaoTest5 = DaoFactory.createSellerDao();
+        Seller seller5 = sellerDaoTest5.findById(1);
+        seller5.setName(Util.generateRandomString());
+        sellerDaoTest5.update(seller5);
+        System.out.println("Updated finished! Id = " + seller5.getId());
     }
 }
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -71,7 +71,26 @@ public class SellerDaoJDBC implements SellerDao {
     }
 
     @Override
-    public void update(Seller obj) {
+    public void update(Seller seller) {
+        PreparedStatement preparedStatement = null;
+        try {
+            preparedStatement = conn.prepareStatement(
+                    "UPDATE seller "
+                            + "SET Name = ?, Email = ?, BirthDate = ?, BaseSalary = ?, DepartmentId = ? "
+                            + "WHERE Id = ?");
+            preparedStatement.setString(1, seller.getName());
+            preparedStatement.setString(2, seller.getEmail());
+            preparedStatement.setDate(3, java.sql.Date.valueOf(seller.getBirthDate()));
+            preparedStatement.setDouble(4, seller.getBaseSalary());
+            preparedStatement.setInt(5, seller.getDepartment().getId());
+            preparedStatement.setInt(6, seller.getId());
+            preparedStatement.executeUpdate();
+
+        } catch (SQLException e) {
+            throw new DBException(e.getMessage());
+        } finally {
+            DB.closeStatement(preparedStatement);
+        }
 
     }
 
